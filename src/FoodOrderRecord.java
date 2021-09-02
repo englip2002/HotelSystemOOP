beb610ba46af9481fc98a14bc8facf68606f087d
EngLip <englip455@gmail.com>
2021-09-02 14:29:15 +0800

    update

--- src/FoodOrderRecord.java
+++ src/FoodOrderRecord.java
@@ -1,4 +1,5 @@
-public class FoodOrderRecord {
+
+public class FoodOrderRecord { // if dont have just use Object then cast
     private FoodOrder[] foodOrderList = new FoodOrder[100];
     private static int orderCount = 0;
 
@@ -10,13 +11,28 @@ public class FoodOrderRecord {
         orderCount++;
     }
 
+    public void generateOrderRecord(String orderID) {
+        int i = 0;
+        boolean orderFound = false;
+        for (i = 0; i < orderCount; i++) {
+            if (foodOrderList[i].getOrderID().equalsIgnoreCase(orderID)) {
+                foodOrderList[i].generateOrderReceipt();
+                orderFound = true;
+            }
+        }
+
+        if (orderFound == false) {
+            System.out.println("Order ID not found!");
+        }
+    }
+
     // make changes use generate order record by Object
     public void generateOrderRecord(Object obj) {
         int i = 0;
         boolean orderFound = false;
         if (obj instanceof Reservation) {
             for (i = 0; i < orderCount; i++) {
-                if (foodOrderList[i].getOrderID().equalsIgnoreCase(((Reservation)obj).getReservationID())) {
+                if (foodOrderList[i].getOrderID().equalsIgnoreCase(((Reservation) obj).getOrderID())) {
                     foodOrderList[i].generateOrderReceipt();
                     orderFound = true;
                 }
