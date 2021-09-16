cc38c47cc49e22cf9c766b09bbcbc5ee41b74f56
SengWaiLee <86587176+SengWaiLee@users.noreply.github.com>
2021-09-16 18:50:40 +0800

    Add files via upload

--- src/Customer.java
+++ src/Customer.java
@@ -1,27 +1,57 @@
-import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class Customer {
-	// Customer is here to avoid compile error, waiting for sengwai to upload the complete ver.
-	private ArrayList<Reservation> reservations;
-	
-	Customer() {
-		reservations = new ArrayList<>();
-	}
-	
-	public void addReservation(Reservation reservation) {
-		reservations.add(reservation);
-	}
-	
-	public void cancelReservation(String reservationID) {
-		for (Reservation each: reservations) {
-			if (each.getReservationID().equals(reservationID)) {
-				each.cancel();
-				break;
-			}
-		}
-	}
-	
-	public ArrayList<Reservation> getReservationList() {
-		return reservations;
-	}
+    private String customerID;
+    private String customerName;
+    private String dateOfBirth;
+    private String customerPassword;
+    private String customerEmail;
+    
+    public Customer(String customerID, String customerName, String dateOfBirth, String customerPassword, String customerEmail){
+        this.customerID = customerID;
+        this.customerName =customerName;
+        this.dateOfBirth = dateOfBirth;
+        this.customerPassword = customerPassword;
+        this.customerEmail = customerEmail;
+    }
+    
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+    
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getCustomerPassword() {
+        return customerPassword;
+    }
+
+    public void setCustomerPassword(String customerPassword) {
+        this.customerPassword = customerPassword;
+    }
+    
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
 }
