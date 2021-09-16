798bc65d6d8d148a7c7ef952351a5c05f4aaa57a
EngLip <englip455@gmail.com>
2021-09-16 22:14:44 +0800

    update

--- src/mainPage.java
+++ src/mainPage.java
@@ -29,7 +29,7 @@ public final class mainPage {
                 System.out.println("Invalid input. Please Enter Again!\n");
             }
             if (option == 1) {
-                login();
+                login(customerArr);
                 drawLine();
             } else if (option == 2) {
                 customerArr[Customer.getCustomerCount()+1] = register();
@@ -55,7 +55,7 @@ public final class mainPage {
                 .println("\t\t\t\t\t\t|__|  |__|\\______/|__|_____|_____|     \\____/|__| \\____/|__|_____| \\/  |__|");
     }
 
-public void login() {
+public static void login(Customer[] customerArr) {
         String loginPassword, loginEmail;
         boolean validLogin = false;
         Scanner customerLogin = new Scanner(System.in);
