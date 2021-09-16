e72e03e719270876eef3d3d63b9214b41d72a357
EngLip <englip455@gmail.com>
2021-09-16 21:51:40 +0800

    update

--- src/mainPage.java
+++ src/mainPage.java
@@ -1,3 +1,4 @@
+
 //Lee Seng Wai
 //2000401
 //DCS2 G1
@@ -5,68 +6,62 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
-import javax.print.event.PrintJobListener;
-
 public final class mainPage {
 
-    private final Customer[] customerArr;
-    Scanner mainPage = new Scanner(System.in);
-
     public static void main(String[] args) {
-        new mainPage();
-    }
-
-    public mainPage() {
-        customerArr = initializeCustomer();
-        int option = 0;
-        int login;
         Scanner mainPage = new Scanner(System.in);
+        Customer[] customerArr = initializeCustomer();
+        mainBanner();
 
+        int option = 0;
         do {
-            System.out.println("\t\t\t\t\t\t __    __  _____ _______ __ __           ____    ___ _________ __     ____ ");
-            System.out.println("\t\t\t\t\t\t|  |  |  |/      \\    __|__|  |         /  __\\  /  /  __\\   __|__|\\  /    |");
-            System.out.println("\t\t\t\t\t\t|  |__|  |   /\\   |  |  |__   |        /  /__ \\/  /  /__   |  |__  \\/     |");
-            System.out.println("\t\t\t\t\t\t|   __   |  |  |  |  |   __|  |        \\ __  \\   /\\ __  \\  |   __|    /|  |");
-            System.out.println("\t\t\t\t\t\t|  |  |  |   \\/   |  |  |__   |__      ___/  /  | ___/  /  |  |__ \\  / |  |");
-            System.out.println("\t\t\t\t\t\t|__|  |__|\\______/|__|_____|_____|     \\____/|__| \\____/|__|_____| \\/  |__|");
-            do {
-                System.out.println();
-                System.out.println("Main Menu");
-                System.out.println("=========");
-                System.out.println("1. Login");
-                System.out.println("2. Register");
-                System.out.println("3. Logout");
-                System.out.println("4. View and edit Customer profile");
-                System.out.println("5. Reservation History");
+            System.out.println();
+            System.out.println("Main Menu");
+            System.out.println("=========");
+            System.out.println("1. Login");
+            System.out.println("2. Register");
+            System.out.println("3. Logout");
+            System.out.println("4. View and edit Customer profile");
+            System.out.println("5. Reservation History");
+            drawLine();
+            System.out.print("Enter your option (1-4) : ");
+            option = mainPage.nextInt();
+
+            if (option < 1 || option > 4) {
+                System.out.println("Invalid input. Please Enter Again!\n");
+            }
+            if (option == 1) {
+                login();
                 drawLine();
-                System.out.print("Enter your option (1-4) : ");
-                option = mainPage.nextInt();
-                mainPage.nextLine();
-                if (option < 1 || option > 4) {
-                    System.out.println("Invalid input. Please Enter Again!\n");
-                }
-                if (option == 1) {
-                    login();
-                    drawLine();
-                }
-                else if (option == 2) {
-                    register();
-                    drawLine();
-                }        
-                else if (option == 3) {
-                    logout();
-                }
-                else if (option == 4) {
-                    editProfile();
-                }
-                else {
-                    reservationHistory();
-                }        
-            } while (option == 1);
+            } else if (option == 2) {
+                register();
+                drawLine();
+            } else if (option == 3) {
+                logout();
+            } else if (option == 4) {
+                editProfile();
+            } else if (option == 5) {
+                reservationHistory();
+            } else {
+                System.out.println("Invalid input. Please Enter Again!\n");
+            }
         } while (option < 1 || option > 4);
     }
 
-    public void login() {
+    public static void mainBanner() {
+        System.out.println("\t\t\t\t\t\t __    __  _____ _______ __ __           ____    ___ _________ __     ____ ");
+        System.out
+                .println("\t\t\t\t\t\t|  |  |  |/      \\    __|__|  |         /  __\\  /  /  __\\   __|__|\\  /    |");
+        System.out
+                .println("\t\t\t\t\t\t|  |__|  |   /\\   |  |  |__   |        /  /__ \\/  /  /__   |  |__  \\/     |");
+        System.out
+                .println("\t\t\t\t\t\t|   __   |  |  |  |  |   __|  |        \\ __  \\   /\\ __  \\  |   __|    /|  |");
+        System.out.println("\t\t\t\t\t\t|  |  |  |   \\/   |  |  |__   |__      ___/  /  | ___/  /  |  |__ \\  / |  |");
+        System.out
+                .println("\t\t\t\t\t\t|__|  |__|\\______/|__|_____|_____|     \\____/|__| \\____/|__|_____| \\/  |__|");
+    }
+
+    public static void login() {
         String customerID, customerName, customerPassword, customerEmail, dateOfBirth;
         boolean validLogin = false;
         Scanner customerLogin = new Scanner(System.in);
@@ -75,13 +70,12 @@ public final class mainPage {
         } while (validLogin == false);
     }
 
-    private Customer[] initializeCustomer() {
-        Customer[] custArr = {new Customer("Gordon Ramsay", "1969-09-06", "Gordon69", "gordonramsay69@hotmail.com"),
-            new Customer("Mohammad Ali", "1987-11-27", "Alibaba", "mohdali@hotmail.com"),
-            new Customer("Ranjeev Singh", "2000-10-08", "RJ2000", "ranjeevsingh@hotmail.com"),
-            new Customer("Leong Kah Jun", "2005-05-05", "LEONGKJ", "kahjunleong@hotmail.com"),
-            new Customer("Jonathan Wong Chou Jin", "12-12-1975", "jonwong1975", "jonathanwong@hotmail.com")
-        };
+    private static Customer[] initializeCustomer() {
+        Customer[] custArr = { new Customer("Gordon Ramsay", "1969-09-06", "Gordon69", "gordonramsay69@hotmail.com"),
+                new Customer("Mohammad Ali", "1987-11-27", "Alibaba", "mohdali@hotmail.com"),
+                new Customer("Ranjeev Singh", "2000-10-08", "RJ2000", "ranjeevsingh@hotmail.com"),
+                new Customer("Leong Kah Jun", "2005-05-05", "LEONGKJ", "kahjunleong@hotmail.com"),
+                new Customer("Jonathan Wong Chou Jin", "12-12-1975", "jonwong1975", "jonathanwong@hotmail.com") };
         return custArr;
     }
 
@@ -93,47 +87,48 @@ public final class mainPage {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter you name : ");
-        registerName=scanner.nextLine();
-        
-        registerDateOfBirth=getDateInput(scanner, "Please enter your Date Of Birth (YYYY-MM-DD): ").toString();
+        registerName = scanner.nextLine();
+
+        registerDateOfBirth = getDateInput(scanner, "Please enter your Date Of Birth (YYYY-MM-DD): ").toString();
         System.out.print("Please enter you password : ");
-        registerPassword=scanner.nextLine();
+        registerPassword = scanner.nextLine();
         System.out.print("Please enter you Email : ");
-        registerEmail=scanner.nextLine();
-        while(validateEmail(registerEmail)==false){
+        registerEmail = scanner.nextLine();
+        while (validateEmail(registerEmail) == false) {
             System.out.println("Invalid Email! Please Try Again");
             System.out.print("Please enter you Email : ");
-            registerEmail=scanner.nextLine();
+            registerEmail = scanner.nextLine();
         }
 
-        Customer customer=new Customer(customerID, customerName, dateOfBirth, customerPassword, customerEmail)
+        Customer customer = new Customer(registerName, registerDateOfBirth, registerPassword, registerEmail);
+        return customer;
     }
 
-    public static boolean validateEmail(String email){
-        boolean validateAt=false;
-        boolean validateDot=false;
-        for(int i=0;i<email.length();i++){
-            if(email.charAt(i)=='@')
-                validateAt=true;
-            if(email.charAt(i)=='.')
-                validateDot=true;
+    public static boolean validateEmail(String email) {
+        boolean validateAt = false;
+        boolean validateDot = false;
+        for (int i = 0; i < email.length(); i++) {
+            if (email.charAt(i) == '@')
+                validateAt = true;
+            if (email.charAt(i) == '.')
+                validateDot = true;
         }
 
-        if(validateAt&&validateDot)
+        if (validateAt && validateDot)
             return true;
         else
             return false;
     }
 
-    public void logout() {
+    public static void logout() {
         System.out.println("Thank you for your service!");
     }
 
-    public void reservationHistory() {
+    public static void reservationHistory() {
         System.out.print("Hello mdfk");
     }
 
-    public void editProfile() {
+    public static void editProfile() {
 
     }
 
@@ -160,4 +155,3 @@ public final class mainPage {
         return d;
     }
 }
-
